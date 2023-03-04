a6c551919d212d2638a19b9cbbf4ed2eae12e76f
antony-chi <antony.cp9@gmail.com>
2023-03-04 12:24:52 -0600

    SongList funcionando

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import SongList from '@/components/SongList'
+import './home.css'
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='izquierdo'>
+        <h2>Lado Izquierdo</h2>
+        <SongList />
+      </div>
+
+      <div className='derecho'>
+        <h2>Lado Derecho</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/index.js
@@ -0,0 +1,6 @@
+import Home from './Home'
+
+export default Home
+
+// esto es igual pero en una sola linea
+// export { default as Home } from './Home'
